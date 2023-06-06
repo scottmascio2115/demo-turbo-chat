4f6e035f333d04582113c7e8515a7ae0e7febe08
Scott Mascio <smascio@covermymeds.com>
2023-06-06 15:51:59 -0400

    fix devise logins and text color

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  after_create :update_user_text_color
 
   has_many :messages
+
+  def name
+    email.split('@').first.capitalize
+  end
+
+  private
+
+  def update_user_text_color
+    self.update(text_color: "#" + "%06x" % (rand * 0xffffff))
+  end
 end
