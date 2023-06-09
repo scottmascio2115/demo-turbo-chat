2f37bd418abd123d3038080ec386da429c008ff7
Scott Mascio <smascio@covermymeds.com>
2023-06-09 12:49:38 -0400

    add and remove logged in users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,30 @@ class User < ApplicationRecord
 
   has_many :messages
 
+  before_update :broadcast_logged_in_user, if: :user_logged_in?
+  before_update :broadcast_logged_out_user, if: :user_logged_out?
+
   def name
     email.split('@').first.capitalize
   end
 
   private
+
+  def broadcast_logged_in_user
+    broadcast_append_to "users_stream", partial: "users/user", locals: { user: self }, target: "users"
+  end
+
+  def broadcast_logged_out_user
+    broadcast_remove_to "users_stream"
+  end
+
+  def user_logged_in?
+    self.logged_in_changed? && self.logged_in == true
+  end
+
+  def user_logged_out?
+    self.logged_in_changed? && self.logged_in == false
+  end
 
   def update_user_text_color
     self.update(text_color: "#" + "%06x" % (rand * 0xffffff))
