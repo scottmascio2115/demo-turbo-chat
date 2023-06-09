6fd615ff22014a109dbe04fde7f19ce03a7f65e5
Scott Mascio <smascio@covermymeds.com>
2023-06-09 09:42:25 -0400

    create messages with turbo streams and action cable

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
   validates :body, presence: true
   belongs_to :user
+
+  after_create_commit -> { broadcast_append_to "messages_stream", partial: "messages/message", locals: { message: self }, target: "messages" }
 end
